1eba08381319808f00f691dce9c77869811fcb53
Sathish Raja E <sathishraja0815@gmail.com>
2025-10-03 14:40:31 +0530

    Update login.js

--- js/login.js
+++ js/login.js
@@ -15,7 +15,7 @@ document.querySelector('form').addEventListener('submit', function (e) {
     } else if (role === "trainer") {
         window.location.href = "trainer_attendance.html";
     } else if (role === "admin") {
-        window.location.href = "admin.html";
+        window.location.href = "Admin.html";
     }
     else if (role === "Coordinator") {
         window.location.href = "Coordinator.html";
@@ -24,5 +24,6 @@ document.querySelector('form').addEventListener('submit', function (e) {
         alert("Invalid role selected!");
     }
 });
+
 
 
